2bed9a90d1cb4ea91dfa47dd29eb67a4f594af35
Maxim Filonov <53992153+sl1depengwyn@users.noreply.github.com>
2023-11-17 14:58:53 +0300

    Slight refactoring

--- src/Game/Game.hs
+++ src/Game/Game.hs
@@ -2,7 +2,6 @@ module Game.Game where
 
 import CodeWorld
 import Data.List
-import Data.Maybe
 import Data.Text qualified as T
 import System.Random
 
@@ -65,31 +64,31 @@ drawGates :: [Gate] -> Picture
 drawGates = pictures . map drawGate
 
 drawPlayer :: Player -> WorldState -> Picture
-drawPlayer Player {y = y', hitBoxSize = r} state = translated 0 y' playerPicture
+drawPlayer Player {y = y, hitBoxSize = r} state = translated 0 y maybeDeadPlayerPicture
   where
-    playerPicture' = rectangle r r <> lettering "\x1F6F8"
-    playerPicture = case state of
-      Fail -> reflected 0 playerPicture'
-      _ -> playerPicture'
+    playerPicture = rectangle r r <> lettering "\x1F6F8"
+    maybeDeadPlayerPicture = case state of
+      Fail -> reflected 0 playerPicture
+      _ -> playerPicture
 
 drawScore :: Int -> Picture
 drawScore score = translated (-5) (-5) (lettering (T.pack (show score)))
 
 onScreen :: Double -> Gate -> Bool
-onScreen globalOffset Gate {offsetX = offsetX'} = (offsetX' + globalOffset) < 50
+onScreen globalOffset Gate {offsetX = offsetX} = (offsetX + globalOffset) < 50
 
 playerShift :: Double
 playerShift = -5
 
 drawWorld :: World -> Picture
-drawWorld World {gates = gates', offset = offset', player = player', score = score', state = state'} =
-  maybeDrawMenu state'
-    <> drawScore score'
+drawWorld World {..} =
+  maybeDrawMenu state
+    <> drawScore score
     <> translated
       playerShift
       0
-      ( drawPlayer player' state'
-          <> translated offset' 0 (drawGates (takeWhile (onScreen offset') gates'))
+      ( drawPlayer player state
+          <> translated offset 0 (drawGates (takeWhile (onScreen offset) gates))
       )
   where
     maybeDrawMenu :: WorldState -> Picture
@@ -120,9 +119,21 @@ newRandomGates = do
   gen <- newStdGen
   activityOf (generateWorld gen) handleEvent drawWorld
 
+handleEvent :: Event -> World -> World
+handleEvent e world@World {state = Progress} = handleProgressEvent e world
+handleEvent e world@World {state = Fail} = handleFailEvent e world
+handleEvent e world@World {state = Idle} = handleIdleEvent e world
+
 handleProgressEvent :: Event -> World -> World
 handleProgressEvent (TimePassing dt) world = updateWorld dt world
-handleProgressEvent (KeyPress " ") world@World {player = player', spacePressed = False} = updateWorld 0 (world {player = player' {velocity = pushAcceleration}, spacePressed = True})
+handleProgressEvent (KeyPress " ") world@World {..}
+  | spacePressed = world
+  | otherwise =
+      updateWorld 0 $
+        world
+          { player = player {velocity = pushAcceleration},
+            spacePressed = True
+          }
 handleProgressEvent (KeyRelease " ") world = updateWorld 0 (world {spacePressed = False})
 handleProgressEvent _ world = world
 
@@ -131,58 +142,56 @@ handleFailEvent (TimePassing dt) world = updateWorld dt world
 handleFailEvent (KeyPress " ") World {generator = g} = generateWorld g
 handleFailEvent _ world = world
 
-handleEvent :: Event -> World -> World
-handleEvent e world@World {state = Progress} = handleProgressEvent e world
-handleEvent e world@World {state = Fail} = handleFailEvent e world
-handleEvent e world@World {state = Idle} = handleIdleEvent e world
-
 handleIdleEvent :: Event -> World -> World
 handleIdleEvent (KeyPress " ") world = world {state = Progress}
 handleIdleEvent _ world = world
 
 offScreen :: Double -> Gate -> Bool
 -- TODO: Check why here is '-6' but not '0'
-offScreen globalOffset Gate {offsetX = offsetX'} = (offsetX' + globalOffset) < -6
+offScreen globalOffset Gate {offsetX = offsetX} = (offsetX + globalOffset) < -6
 
 updateWorld :: Double -> World -> World
-updateWorld dt world@World {state = Fail, player = player'} = newStaticWorld
+updateWorld dt world@World {state = Fail, player = player} = newStaticWorld
   where
-    newFailedPlayer = updatePlayer dt player'
+    newFailedPlayer = updatePlayer dt player
     newStaticWorld = world {speed = 0, player = newFailedPlayer}
-updateWorld dt world@World {time = time', offset = offset', player = player', gates = gates', speed = speed', score = score'} =
-  newWorld
+updateWorld dt world@World {..} =
+  newWorldWithPlayer
   where
-    newOffset = offset' - dt * speed'
-    newWorld' = world {time = time' + dt, offset = newOffset, speed = max (speed' + worldSpeedIncrease) maxWorldSpeed}
-    screenGates = takeWhile (onScreen offset') gates' -- TODO: consider only gates with offset <= player's position
-    scoreImprovement = calculateScoreImprovement offset' newOffset screenGates
-    newState = if isFailed newWorld' then Fail else Progress
-    newPlayer' = case newState of
-      Fail -> player' {velocity = pushAcceleration / 2}
-      _ -> player'
-    newPlayer = updatePlayer dt newPlayer'
+    newOffset = offset - dt * speed
+    newWorld = world {time = time + dt, offset = newOffset, speed = max (speed + worldSpeedIncrease) maxWorldSpeed}
+    screenGates = takeWhile (onScreen offset) gates -- TODO: consider only gates with offset <= player's position
+    scoreImprovement = calculateScoreImprovement offset newOffset screenGates
+    newState = if isFailed newWorld then Fail else Progress
+    newPlayer =
+      updatePlayer
+        dt
+        ( case newState of
+            Fail -> player {velocity = pushAcceleration / 2}
+            _ -> player
+        )
 
-    newWorld = newWorld' {state = newState, gates = dropWhile (offScreen offset') gates', score = score' + scoreImprovement, player = newPlayer}
+    newWorldWithPlayer = newWorld {state = newState, gates = dropWhile (offScreen offset) gates, score = score + scoreImprovement, player = newPlayer}
 
 calculateScoreImprovement :: Double -> Double -> [Gate] -> Int
 calculateScoreImprovement oldOffset newOffset gates =
   length (filter (\Gate {offsetX = offset'} -> oldOffset + offset' > 0 && offset' + newOffset <= 0) gates)
 
 isFailed :: World -> Bool
-isFailed world@World {gates = gates', offset = offset'} = any (isCollided world) (takeWhile (onScreen offset') gates')
+isFailed world@World {gates = gates, offset = offset} = any (isCollided world) (takeWhile (onScreen offset) gates)
 
 -- For better UX
 collisionEpsilon :: Double
 collisionEpsilon = 0.1
 
 isCollided :: World -> Gate -> Bool
-isCollided World {offset = worldOffset, player = Player {y = y', hitBoxSize = r}} Gate {offsetX = offsetX', offsetY = offsetY', width = width', height = height'}
-  | abs y' > 10 = True -- TODO: create constant
-  | ( (y' + rHalf) > (offsetY' + (height' / 2) + collisionEpsilon)
-        || (y' - rHalf) < (offsetY' - (height' / 2) - collisionEpsilon)
+isCollided World {offset = worldOffset, player = Player {y = y, hitBoxSize = r}} Gate {offsetX = offsetX, offsetY = offsetY, width = width, height = height}
+  | abs y > 10 = True -- TODO: create constant
+  | ( (y + rHalf) > (offsetY + (height / 2) + collisionEpsilon)
+        || (y - rHalf) < (offsetY - (height / 2) - collisionEpsilon)
     )
-      && (-worldOffset + rHalf) > (offsetX' - width' / 2 + collisionEpsilon)
-      && (-worldOffset - rHalf) < (offsetX' + width' / 2 - collisionEpsilon) =
+      && (-worldOffset + rHalf) > (offsetX - width / 2 + collisionEpsilon)
+      && (-worldOffset - rHalf) < (offsetX + width / 2 - collisionEpsilon) =
       True
   | otherwise = False
   where
@@ -191,11 +200,11 @@ isCollided World {offset = worldOffset, player = Player {y = y', hitBoxSize = r}
 updatePlayer :: Double -> Player -> Player
 updatePlayer
   dt
-  player@Player {velocity = velocity', y = y'} =
+  player@Player {velocity = velocity, y = y} =
     player {velocity = newVelocity, y = newY}
     where
-      newVelocity = velocity' + dt * gravity
-      newY = max (y' + velocity' * dt) (-12)
+      newVelocity = velocity + dt * gravity
+      newY = max (y + velocity * dt) (-12)
 
 data Player = Player
   { velocity :: Double,
